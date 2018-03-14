d51f3f2655c7a6a561f129d1de52cfeab8e645a5
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-03-14 16:25:33 -0400

    Refactoring code

--- src/atmos_logs/core.clj
+++ src/atmos_logs/core.clj
@@ -8,6 +8,21 @@
             [clj-time.format :refer [formatter formatter-local unparse]])
   (:import (clojure.lang PersistentArrayMap)))
 
+
+(def ^:private date-format "yyyy-MM-dd HH:mm:ss")
+(def ^:private joda-date-formatter (formatter date-format))
+
+(defn joda-time-zone-date-formatter
+  [format]
+  (formatter-local format))
+
+(defn utc-now [] (unparse joda-date-formatter (now)))
+
+(defn convert-time-zone
+  [date time-zone format]
+  (unparse (joda-time-zone-date-formatter format) (to-time-zone (from-sql-time date) time-zone)))
+
+
 (defprotocol ILogRepository
   (add-log [data]))
 
--- src/atmos_logs/logs/core.clj
+++ src/atmos_logs/logs/core.clj
@@ -7,19 +7,6 @@
            (entity-fields :id :user_entity_id :log_type :date :module :note))
 
 
-(def ^:private date-format "yyyy-MM-dd HH:mm:ss")
-(def ^:private joda-date-formatter (formatter date-format))
-
-(defn joda-time-zone-date-formatter
-  [format]
-  (formatter-local format))
-
-(defn utc-now [] (unparse joda-date-formatter (now)))
-
-(defn convert-time-zone
-  [date time-zone format]
-  (unparse (joda-time-zone-date-formatter format) (to-time-zone (from-sql-time date) time-zone)))
-
 ;------------------------------
 ; BEGIN Logs functions
 ;------------------------------
