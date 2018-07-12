d3d7a51890625683fcefaceed02488f77332a658
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-07-12 18:11:48 -0400

    #1 | Refactoring unit testing

--- test/atmos_logs/core_test.clj
+++ test/atmos_logs/core_test.clj
@@ -1,22 +1,16 @@
 (ns atmos-logs.core-test
   (:require [clojure.test :refer :all]
-            [atmos-logs.core :refer :all]
-            [atmos-kernel.core :refer [read-resource-edn]]
-            [atmos-rdb-kernel.core :refer [defpersistence init-persistence]]))
+            [atmos-logs.service :refer :all]))
 
-(def configuration (read-resource-edn :config-dev))
-
-(-> configuration :database defpersistence init-persistence)
 
 (deftest log-repository-testing
   (let [mock-log {:user_entity_id 1
                   :log_type       "INFO"
-                  :date           (utc-now-string)
                   :module         "Mock Module"
                   :note           "Mock note"}
         test-id 1
-        id-inserted (add-log mock-log)]
+        id-inserted (add-logs mock-log)]
     (testing "Insert data"
       (is (number? id-inserted)))
     (testing "Retrieve single data"
-      (is (= test-id (-> test-id get-log :id))))))
+      (is (= test-id (-> test-id get-logs :id))))))
