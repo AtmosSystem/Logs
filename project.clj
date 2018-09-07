f5ae1f248311d8e7e60d23e41514002188a64add
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-09-07 18:19:21 -0400

    Change auth

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject atmos-logs "1.5"
+(defproject atmos-logs "1.6"
   :description "Logs micro service for atmos system"
   :url "https://github.com/AtmosSystem/Logs"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [atmos-kernel "0.6.34"]]
+                 [atmos-kernel "0.6.35"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
