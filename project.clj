2c876408f29b1eda51aef30014d42edd83ea4c7c
Santiago de Pedro <santiago.depedro@numerade.com>
2022-12-13 20:33:23 -0400

    Dependencies
    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject atmos-logs "2.5"
+(defproject atmos-logs "3.0-SNAPSHOT"
   :description "Logs micro service for atmos system"
   :url "https://github.com/AtmosSystem/Logs"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [atmos-kernel "1.5"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [atmos-kernel "2.2"]
+                 [atmos-web-kernel-reitit "1.0"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
