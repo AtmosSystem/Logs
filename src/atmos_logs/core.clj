dfddb6c0d7a5f7bcb3a8d1f494e800dd0a5e7b09
Santiago de Pedro <santiago.pedro23@gmail.com>
2022-12-26 14:59:01 -0400

    Methods
    Add trace and warn methods

--- src/atmos_logs/core.clj
+++ src/atmos_logs/core.clj
@@ -1,13 +1,15 @@
 (ns atmos-logs.core)
 
-(def log-types [::info ::debug ::error ::exception ::fatal])
+(def log-types [::info ::trace ::warn ::debug ::error ::exception ::fatal])
 
 (defprotocol IPersistenceLogger
     "Protocol to persist log data"
-    (add-log [log]))
+    (log-> [log]))
 
 (defprotocol ILoggerActions
     (info [data])
+    (trace [data])
+    (warn [data])
     (debug [data])
     (error [data])
     (exception [data])
