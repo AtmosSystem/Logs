96cf3158c5f2ea7463998213ac09799f00825c90
Santiago de Pedro <santiago.pedro23@gmail.com>
2023-10-27 12:09:29 -0400

    Project: Versions
    Bump dependencies and project versions

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject org.clojars.atmos-system/atmos-logs "3.0-SNAPSHOT"
+(defproject org.clojars.atmos-system/atmos-logs "3.0"
     :description "Logs micro service for atmos system"
     :url "https://github.com/AtmosSystem/Logs"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.11.1"]
-                   [org.clojars.atmos-system/atmos-kernel "2.2-SNAPSHOT"]
-                   [org.clojars.atmos-system/atmos-web-kernel-reitit "2.0-SNAPSHOT"]]
-    :deploy-repositories [["clojars" {:url      "https://repo.clojars.org/"
-                                      :username :env/clojars_username
-                                      :password :env/clojars_password}]])
+                   [org.clojars.atmos-system/atmos-kernel "2.2"]
+                   [org.clojars.atmos-system/atmos-web-kernel-reitit "2.0"]]
+    :deploy-repositories [["clojars" {:sign-releases false
+                                      :url           "https://repo.clojars.org/"
+                                      :username      :env/clojars_username
+                                      :password      :env/clojars_password}]])
