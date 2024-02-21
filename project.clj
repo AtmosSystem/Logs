71eb1a9d7d0dc2e260d1d9d30f71face65ad887c
Santiago de Pedro <santiago.pedro23@gmail.com>
2024-02-21 11:03:10 -0400

    Update Atmos logs to a library and refactor core functionality
    The commit modifies Atmos logs from a microservice to a library for system activity logging. The README.md and project.clj files were updated to reflect this change. project.clj's version was updated to 3.5 and unnecessary dependencies were removed. In core.clj, logger protocols and spec definitions are redesigned, while api.clj is deleted due to the restructure. Changes were likewise made to spec.clj to conform with the core functionalities’ new specifications.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.atmos-system/atmos-logs "3.0"
-    :description "Logs micro service for atmos system"
+(defproject org.clojars.atmos-system/atmos-logs "3.5"
+    :description "Log library for atmos system"
     :url "https://github.com/AtmosSystem/Logs"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.11.1"]
-                   [org.clojars.atmos-system/atmos-kernel "2.2"]
-                   [org.clojars.atmos-system/atmos-web-kernel-reitit "2.0"]]
+                   [org.clojars.atmos-system/atmos-kernel "2.2"]]
+    :profiles {:dev  {:dependencies [[org.clojure/test.check "0.9.0"]]}}
     :deploy-repositories [["clojars" {:sign-releases false
                                       :url           "https://repo.clojars.org/"
                                       :username      :env/clojars_username
